5eb45fc6ba8a44fdc61cd41cd755aa2e33d33dd0
sachin123jadhav <sachin123jadhav@gmail.com>
2024-02-12 17:51:18 +0530

    changes done

--- assets/js/script.js
+++ assets/js/script.js
@@ -642,3 +642,19 @@ function unflipCard(card) {
 
 
  
+
+  let items = document.querySelectorAll(".carousel .carousel-item");
+
+  items.forEach((el) => {
+    const minPerSlide = 6;
+    let next = el.nextElementSibling;
+    for (var i = 1; i < minPerSlide; i++) {
+      if (!next) {
+        // wrap carousel by using first child
+        next = items[0];
+      }
+      let cloneChild = next.cloneNode(true);
+      el.appendChild(cloneChild.children[0]);
+      next = next.nextElementSibling;
+    }
+  });
